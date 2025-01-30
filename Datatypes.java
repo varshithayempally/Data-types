9cc6a3f6e6c07ac53f5d98fab58a17e49c7c029e
varshithayempally <varshithayempally@gmail.com>
2025-01-30 21:25:55 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte bytevar=100;
+        System.out.println("byte value:"+bytevar);
+        short shortvar=10000;
+        System.out.println("short value:"+shortvar);
+        int intvar=100000;
+        System.out.println("int value:"+intvar);
+        long longvar=10000000000L;
+        System.out.println("long value:"+longvar);
+        float floatvar=10.5F;
+        System.out.println("float value:"+floatvar);
+        double doublevar=10.55555;
+        System.out.println("double value:"+doublevar);
+        char charvar='A';
+        System.out.println("char value:"+charvar);
+        boolean booleanvar=true;
+        System.out.println("boolean value:"+booleanvar);
+    }
+    
+}
